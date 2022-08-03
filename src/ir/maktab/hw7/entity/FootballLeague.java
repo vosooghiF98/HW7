d3054635ac845b2eb78479ddecd471c0c3fd1db5
farshid vosooghi <vosooghifarshid95@gmail.com>
2022-08-03 22:29:23 +0430

    update code

--- src/ir/maktab/hw7/entity/FootballLeague.java
+++ src/ir/maktab/hw7/entity/FootballLeague.java
@@ -7,30 +7,33 @@ public class FootballLeague extends LeagueList {
 
     @Override
     public void setRace(String fTeam1, int goal1, String fTeam2, int goal2) {
-        if (contains(fTeam1) && contains(fTeam2) && !Objects.equals(fTeam1, fTeam2)) {
-            SportClub team1 = findByName(fTeam1);
-            SportClub team2 = findByName(fTeam2);
-            team1.numberOfGames++;
-            team2.numberOfGames++;
-            if (goal1 > goal2){
-                team1.numberOfWins++;
-                team2.numberOfLosses++;
-                team1.score += 3;
-            }else if (goal1 == goal2){
-                team1.numberOfDraw++;
-                team2.numberOfDraw++;
-                team1.score++;
-                team2.score++;
-            }else {
-                team2.numberOfWins++;
-                team1.numberOfLosses++;
-                team2.score += 3;
+        if (!Objects.equals(fTeam1, fTeam2)) {
+            if (contains(fTeam1) && contains(fTeam2)) {
+                SportClub team1 = findByName(fTeam1);
+                SportClub team2 = findByName(fTeam2);
+                team1.numberOfGames++;
+                team2.numberOfGames++;
+                if (goal1 > goal2) {
+                    team1.numberOfWins++;
+                    team2.numberOfLosses++;
+                    team1.score += 3;
+                } else if (goal1 == goal2) {
+                    team1.numberOfDraw++;
+                    team2.numberOfDraw++;
+                    team1.score++;
+                    team2.score++;
+                } else {
+                    team2.numberOfWins++;
+                    team1.numberOfLosses++;
+                    team2.score += 3;
+                }
+            } else {
+                System.out.println("One or both of clubs not exist!");
             }
+
         }else {
-            System.out.println("One or both of clubs not exist!");
+            System.out.println("Both of clubs name is equal!");
         }
-
     }
-
 
 }
